7cc6d2206d96fd4b5ab071a5ea7b16f1853c1c6c
rokydas <droky136@gmail.com>
2020-09-12 19:47:10 +0600

    firebase config hide

--- src/Components/Shipment/Shipment.js
+++ src/Components/Shipment/Shipment.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Shipment = () => {
-    return (
-        <div>
-            <h1>This is shipment</h1>
-        </div>
-    );
+    const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = data => console.log(data);
+
+  console.log(watch("example")); // watch input value by passing the name of it
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input name="example" defaultValue="test" ref={register} /><br/>
+      <input name="exampleRequired" ref={register({ required: true })} /><br/>
+      {errors.exampleRequired && <span>This field is required</span>} <br/>
+      <input type="submit" />
+    </form>
+  );
 };
 
 export default Shipment;
